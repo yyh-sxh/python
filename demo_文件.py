0014d208badc52da56adbfbf7ab3bf8c4aca44af
yyh <912124728@qq.com>
2022-07-27 23:16:52 +0800

    1

--- demo_文件.py
+++ demo_文件.py
@@ -39,3 +39,13 @@ import os
 # os.rmdir(path) #删除目录
 # os.removedirs(path1/path2...) #删除多级目录
 # os.chdir(path) #将path设置为当前工作目录
+
+# os.path的相关操作函数
+import os.path
+# print(os.path.abspath('demo_文件.py')) #用于获取文件或目录的绝对路径
+# print(os.path.exists('demo_文件.py')) #判断目录或文件是否存在，true/false
+# print(os.path.join('demo_文件.py','name')) #将目录与目录或文件名拼接起来
+# print(os.path.splitext('demo_文件.py')) #将文件名和扩展名分隔
+# print(os.path.basename('demo_文件.py')) #从目录从提取文件名
+# print(os.path.dirname('python\demo_文件.py')) #从一个路径中提取文件路径，不包括文件名
+# print(os.path.isdir('demo_文件.py')) #用于判断是否为路径
